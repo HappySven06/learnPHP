0a728cddeaf86a6973ba43c563176b7bb92eee66
Sven Laht <Sven.Laht@gmail.com>
2024-09-10 15:05:07 +0300

    simple router

--- views/us.php
+++ views/us.php
@@ -3,7 +3,7 @@
 <main class="container">
   <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
     <div class="col-lg-6 px-0">
-      <h1 class="display-4 fst-italic">US taxses fucked up</h1>
+      <h1 class="display-4 fst-italic">U.S tax system is fucked up</h1>
       <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
       <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Continue reading...</a></p>
     </div>
@@ -245,5 +245,4 @@
   </div>
 
 </main>
-
 <?php include 'partials/footer.php'; ?>
