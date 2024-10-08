c8d9cb893bc407ade036dbcccf311da90a7d88b6
Sven Laht <Sven.Laht@gmail.com>
2024-10-08 11:36:21 +0300

    composer

--- public/index.php
+++ public/index.php
@@ -4,11 +4,7 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
-
-spl_autoload_register(function($class){
-    $class=substr($class, 4);
-    require_once __DIR__ . "/../src/$class.php";
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 require __DIR__ . '/../helpers.php';
 require __DIR__ . '/../routes.php';
